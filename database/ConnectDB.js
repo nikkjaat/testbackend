79ae17f12ca17d9514a581a63d2e605bf4726c61
Nikhil <nikhiljaat330@gmail.com>
2025-06-22 09:24:17 +0530

    update

--- database/ConnectDB.js
+++ database/ConnectDB.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(
-    "mongodb+srv://nikhil:nikhil123@scriptguru.wvxesfd.mongodb.net/?retryWrites=true&w=majority&appName=Scriptguru"
-  );
+  await mongoose.connect(process.env.MONGO_URI);
   console.log("Connected to Database successfully");
   const fetched_data = mongoose.connection.db.collection("food_items");
   fetched_data.find({}).toArray((err, data) => {
